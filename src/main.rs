d479ce4fe196d7cbe15c95a2183a23abd532b3a7
jl <jonathan.lepsoey@appear.net>
2023-08-30 12:25:31 +0200

    (main) Add a visitor list to determine access

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,24 @@ fn what_is_your_name() -> String {
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
+    your_name.trim().to_lowercase()
 }
 
 fn main() {
+    let visitor_list: [&str; 2] = ["test", "jonathan"];
+
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name);
+    let mut allow_them_in = false;
+
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+    if allow_them_in {
+        println!("Welcome!");
+    } else {
+        println!("You are not welcome here!");
+    }
 }
