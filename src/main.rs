a171ee00c6a9cdeb9cf6540bfa72b87ceb99a9b7
jl <jonathan.lepsoey@appear.net>
2023-08-30 12:55:44 +0200

    (treehouse) Use loop and vector

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String,
@@ -27,30 +28,28 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list: [Visitor; 2] = [
+    let mut visitor_list = vec![
         Visitor::new("test", "You are just a test, but welcome"),
         Visitor::new("jonathan", "You are our most prized member!"),
     ];
 
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
-    let mut allow_them_in = false;
+    loop {
+        println!("Hello, what's your name?");
+        let name = what_is_your_name();
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
 
-    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
-
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("{} is not on the visitor list.", name),
-    }
-
-    for visitor in &visitor_list {
-        if visitor.name == name {
-            allow_them_in = true;
-            visitor.greet_visitor();
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                }
+            }
         }
-    }
 
-    if !allow_them_in {
-        println!("You are not welcome here!");
+        println!("{:?}", visitor_list);
     }
 }
